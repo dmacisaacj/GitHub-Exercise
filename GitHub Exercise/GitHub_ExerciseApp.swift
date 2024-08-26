37a5c1aefdc18514753069c4fb16e77a682177a9
Dean MacIsaac <deanjr@macisaac.com>
2024-08-26 00:01:21 -0400

    Initial Commit

---
+++ GitHub Exercise/GitHub_ExerciseApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHub_ExerciseApp.swift
+//  GitHub Exercise
+//
+//  Created by Dean MacIsaac on 8/26/24.
+//
+
+import SwiftUI
+
+@main
+struct GitHub_ExerciseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GitHub ExerciseTests/GitHub_ExerciseTests.swift
@@ -0,0 +1,17 @@
+//
+//  GitHub_ExerciseTests.swift
+//  GitHub ExerciseTests
+//
+//  Created by Dean MacIsaac on 8/26/24.
+//
+
+import Testing
+@testable import GitHub_Exercise
+
+struct GitHub_ExerciseTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
